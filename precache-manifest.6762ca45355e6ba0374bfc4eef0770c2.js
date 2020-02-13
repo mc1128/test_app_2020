69bad3310747c8b395d38a2315713b84c8709874
mc1128 <mc1128@naver.com>
2020-02-13 16:14:36 +0900

    Updates

---
+++ precache-manifest.6762ca45355e6ba0374bfc4eef0770c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7f2ed33580cdcf82c93007a84eb0cb",
+    "url": "/test_app_2020/index.html"
+  },
+  {
+    "revision": "d3e761b1a92b61b28818",
+    "url": "/test_app_2020/static/css/main.6cf98f3b.chunk.css"
+  },
+  {
+    "revision": "e96a17e56b6cff4062e5",
+    "url": "/test_app_2020/static/js/2.39ea4a94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test_app_2020/static/js/2.39ea4a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e761b1a92b61b28818",
+    "url": "/test_app_2020/static/js/main.a1d8b6fb.chunk.js"
+  },
+  {
+    "revision": "e6fc4f4bd70eff7113c2",
+    "url": "/test_app_2020/static/js/runtime-main.5332b30d.js"
+  }
+]);
